a961c6bf5e56f3b6fc3c182795ff1564ad31064b
="Justas <justas963@gmail.com>
2016-02-06 19:51:58 +0200

    Aš šito dar nebuvau rašęs? O kas jame turėtų būt? :/

---
+++ src/main/java/lt/ltrp/job/drugdealer/DrugDealerJob.java
@@ -0,0 +1,14 @@
+package lt.ltrp.job.drugdealer;
+
+import lt.ltrp.job.ContractJob;
+import lt.ltrp.job.Job;
+
+/**
+ * @author Bebras
+ *         2016.02.06.
+ */
+public class DrugDealerJob extends ContractJob {
+
+
+
+}
