16d991af2347ea8a0bf2c14d1e3b2fbfee4adda9
="Justas <justas963@gmail.com>
2016-02-13 17:45:45 +0200

    error žinutės jei neranda funkcijų\

--- src/main/java/lt/ltrp/plugin/mapandreas/MapAndreas.java
+++ src/main/java/lt/ltrp/plugin/mapandreas/MapAndreas.java
@@ -4,7 +4,8 @@ import lt.ltrp.Util.PawnFunc;
 import lt.ltrp.plugin.PluginWrapper;
 import net.gtaun.shoebill.amx.AmxCallable;
 import net.gtaun.shoebill.amx.types.ReferenceFloat;
-import net.gtaun.util.event.EventManager;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * @author Bebras
@@ -12,13 +13,16 @@ import net.gtaun.util.event.EventManager;
  */
 public class MapAndreas extends PluginWrapper {
 
+    private static final Logger logger = LoggerFactory.getLogger(MapAndreas.class);
 
     // native MapAndreas_Init(mode, name[]="", len=sizeof(name));
     public static void Init(MapAndreasMode mode, String name) {
         AmxCallable MapAndreas_Init = PawnFunc.getNativeMethod("MapAndreas_Init");
         if(MapAndreas_Init != null) {
             MapAndreas_Init.call(mode.getValue(), name, name.length());
-        }
+        } else
+            logger.error("MapAndreas_Init method not found");
+
     }
 
     public static void Init(MapAndreasMode mode) {
@@ -31,7 +35,8 @@ public class MapAndreas extends PluginWrapper {
         AmxCallable MapAndreas_Unload = PawnFunc.getNativeMethod("MapAndreas_Unload");
         if(MapAndreas_Unload != null) {
             MapAndreas_Unload.call();
-        }
+        } else
+            logger.error("MapAndreas_Unload method not found");
     }
 
 
@@ -43,6 +48,8 @@ public class MapAndreas extends PluginWrapper {
             ReferenceFloat refZ = new ReferenceFloat(z);
             MapAndreas_FindZ_For2DCoord.call(x, y, refZ);
             z = refZ.getValue();
+        } else {
+            logger.error("MapAndreas_FindZ_For2DCoord method not found");
         }
         return z;
     }
@@ -55,6 +62,8 @@ public class MapAndreas extends PluginWrapper {
             ReferenceFloat refZ = new ReferenceFloat(z);
             MapAndreas_FindAverageZ.call(x, y, refZ);
             z = refZ.getValue();
+        } else {
+            logger.error("MapAndreas_FindAverageZ method not found");
         }
         return z;
     }
@@ -64,6 +73,8 @@ public class MapAndreas extends PluginWrapper {
         AmxCallable MapAndreas_SetZ_For2DCoord = PawnFunc.getNativeMethod("MapAndreas_SetZ_For2DCoord");
         if(MapAndreas_SetZ_For2DCoord != null) {
             MapAndreas_SetZ_For2DCoord.call(x, y, z);
+        } else {
+            logger.error("MapAndreas_SetZ_For2DCoord method not found");
         }
     }
 
@@ -72,6 +83,8 @@ public class MapAndreas extends PluginWrapper {
         AmxCallable MapAndreas_SaveCurrentHMap = PawnFunc.getNativeMethod("MapAndreas_SaveCurrentHMap");
         if(MapAndreas_SaveCurrentHMap != null) {
             MapAndreas_SaveCurrentHMap.call(name);
+        } else {
+            logger.error("MapAndreas_SaveCurrentHMap method not found");
         }
     }
 
@@ -80,6 +93,8 @@ public class MapAndreas extends PluginWrapper {
         AmxCallable MapAndreas_GetAddress = PawnFunc.getNativeMethod("MapAndreas_GetAddress");
         if(MapAndreas_GetAddress != null) {
             return (Integer)MapAndreas_GetAddress.call();
+        } else {
+            logger.error("MapAndreas_GetAddress method not found");
         }
         return 0;
     }
