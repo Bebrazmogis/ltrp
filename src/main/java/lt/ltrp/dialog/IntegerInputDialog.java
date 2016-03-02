ba24f6a21fa0a6cc0143ca1e6d7b284354adae85
="Justas <justas963@gmail.com>
2016-03-02 19:57:05 +0200

    Atnaujintas IntegerInputdialog, dabar galima ir naudoti dialog.create()........build() stilių.

--- src/main/java/lt/ltrp/dialog/IntegerInputDialog.java
+++ src/main/java/lt/ltrp/dialog/IntegerInputDialog.java
@@ -1,6 +1,7 @@
 package lt.ltrp.dialog;
 
 import lt.ltrp.player.LtrpPlayer;
+import net.gtaun.shoebill.common.dialog.DialogTextSupplier;
 import net.gtaun.shoebill.common.dialog.InputDialog;
 import net.gtaun.shoebill.object.Player;
 import net.gtaun.util.event.EventManager;
@@ -13,11 +14,19 @@ import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 public class IntegerInputDialog extends InputDialog {
 
     private ClickOkHandler handler;
+    private InputErrorHandler inputErrorHandler;
 
     public IntegerInputDialog(LtrpPlayer player, EventManager parentEventManager) {
         super(player, parentEventManager);
     }
 
+    public static AbstractIntegerInputDialogBuilder<?, ?> create(LtrpPlayer player, EventManager parentEventManager) {
+        return new IntegerInputDialogBuilder(player, parentEventManager);
+    }
+
+    public void setInputErrorHandler(InputErrorHandler h) {
+        this.inputErrorHandler = h;
+    }
 
     @Override
     @Deprecated
@@ -36,6 +45,8 @@ public class IntegerInputDialog extends InputDialog {
             if(handler != null)
                 handler.onClickOk(this, val);
         } catch(NumberFormatException e) {
+            if(inputErrorHandler != null)
+                inputErrorHandler.onInputError(this, text);
             show();
         }
     }
@@ -43,6 +54,51 @@ public class IntegerInputDialog extends InputDialog {
     @FunctionalInterface
     public interface ClickOkHandler {
         void onClickOk(IntegerInputDialog dialog, int val);
+    }
+
+    @FunctionalInterface
+    public interface InputErrorHandler {
+        void onInputError(IntegerInputDialog dialog, String input);
+    }
+
+    @SuppressWarnings("unchecked")
+    public static abstract class AbstractIntegerInputDialogBuilder
+            <DialogType extends IntegerInputDialog, DialogBuilderType extends AbstractIntegerInputDialogBuilder<DialogType, DialogBuilderType>>
+            extends AbstractDialogBuilder<DialogType, DialogBuilderType> {
+        protected AbstractIntegerInputDialogBuilder(DialogType dialog) {
+            super(dialog);
+        }
+
+        public DialogBuilderType message(String message) {
+            dialog.setMessage(message);
+            return (DialogBuilderType) this;
+        }
+
+        public DialogBuilderType message(DialogTextSupplier messageSupplier) {
+            dialog.setMessage(messageSupplier);
+            return (DialogBuilderType) this;
+        }
+
+        public DialogBuilderType onInputError(InputErrorHandler handler1) {
+            dialog.setInputErrorHandler(handler1);
+            return (DialogBuilderType) this;
+        }
+
+        public DialogBuilderType onClickOk(ClickOkHandler handler) {
+            dialog.setClickOkHandler(handler);
+            return (DialogBuilderType) this;
+        }
+
+        public DialogBuilderType line(String line) {
+            dialog.addLine(line);
+            return (DialogBuilderType) this;
+        }
+    }
+
+    public static class IntegerInputDialogBuilder extends AbstractIntegerInputDialogBuilder<IntegerInputDialog, IntegerInputDialogBuilder> {
+        private IntegerInputDialogBuilder(LtrpPlayer player, EventManager parentEventManager) {
+            super(new IntegerInputDialog(player, parentEventManager));
+        }
     }
 
 }
