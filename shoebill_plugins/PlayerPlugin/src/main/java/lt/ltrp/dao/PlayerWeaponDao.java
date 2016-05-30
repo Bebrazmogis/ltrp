3acf408dc9d9f88e48f352717293ced5bd5890ae
="Justas <justas963@gmail.com>
2016-05-30 22:35:24 +0300

    Žaidėjo nešiojamų ginklų DAO.

---
+++ shoebill_plugins/PlayerPlugin/src/main/java/lt/ltrp/dao/PlayerWeaponDao.java
@@ -0,0 +1,17 @@
+package lt.ltrp.dao;
+
+import lt.ltrp.data.LtrpWeaponData;
+import lt.ltrp.object.LtrpPlayer;
+
+/**
+ * @author Bebras
+ *         2016.05.30.
+ */
+public interface PlayerWeaponDao {
+
+
+    int insert(LtrpPlayer player, LtrpWeaponData weapon);
+    void remove(LtrpWeaponData weaponData);
+    LtrpWeaponData[] get(LtrpPlayer player);
+
+}
