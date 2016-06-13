31972aaf980e6bacab1f90af3a45207f4d8de3c5
="Justas <justas963@gmail.com>
2016-06-13 16:52:46 +0300

    WalkStyle ir TalkSTyle gavo po UUID ir statinį metodą gavimui pagal jį

--- shoebill_plugins/LtrpApi/src/main/java/lt/ltrp/constant/TalkStyle.java
+++ shoebill_plugins/LtrpApi/src/main/java/lt/ltrp/constant/TalkStyle.java
@@ -8,19 +8,33 @@ import lt.ltrp.data.Animation;
  */
 public enum TalkStyle {
 
-    CHAT1(new Animation("PED", "IDLE_CHAT", 4.1f, true, true, true, false, true, 0, true)),
-    CHAT2(new Animation("MISC", "IDLE_CHAT_02", 4.1f, true, true, true, false, true, 0, true)),
-    GANG1(new Animation("GHANDS", "GSIGN1", 4.1f, true, true, true, false, true, 0, true)),
-    GANG2(new Animation("GANGS", "PRTIAL_GNGTLKD", 4.1f, true, true, true, false, true, 0, true)),
-    GANG3(new Animation("GANGS", "PRTIAL_GNGTLKH", 4.1f, true, true, true, false, true, 0, true));
+    CHAT1(1, new Animation("PED", "IDLE_CHAT", 4.1f, true, true, true, false, true, 0, true)),
+    CHAT2(2, new Animation("MISC", "IDLE_CHAT_02", 4.1f, true, true, true, false, true, 0, true)),
+    GANG1(3, new Animation("GHANDS", "GSIGN1", 4.1f, true, true, true, false, true, 0, true)),
+    GANG2(4, new Animation("GANGS", "PRTIAL_GNGTLKD", 4.1f, true, true, true, false, true, 0, true)),
+    GANG3(5, new Animation("GANGS", "PRTIAL_GNGTLKH", 4.1f, true, true, true, false, true, 0, true));
 
     Animation animation;
+    int id;
 
-    TalkStyle(Animation animation) {
+    TalkStyle(int id, Animation animation) {
         this.animation = animation;
+        this.id = id;
     }
 
     public Animation getAnimation() {
         return animation;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    static TalkStyle getById(int id) {
+        for(TalkStyle w : values()) {
+            if(w.getId() == id)
+                return w;
+        }
+        return null;
     }
 }
--- shoebill_plugins/LtrpApi/src/main/java/lt/ltrp/constant/WalkStyle.java
+++ shoebill_plugins/LtrpApi/src/main/java/lt/ltrp/constant/WalkStyle.java
@@ -8,22 +8,37 @@ import lt.ltrp.data.Animation;
  */
 public enum WalkStyle {
 
-    GANG1(new Animation("PED", "WALK_GANG1", 4.1f, true, true, true, false, true, 0, true)),
-    GANG2(new Animation("PED", "WALK_GANG2", 4.1f, true, true, true, false, true, 0, true)),
-    FAT(new Animation("PED", "WALK_FATWALK", 4.1f, true, true, true, false, true, 0, true)),
-    WUZI(new Animation("PED", "WUZI_WALK", 4.1f, true, true, true, false, true, 0, true)),
-    WUZI2(new Animation("PED", "WALK_WUZI", 4.1f, true, true, true, false, true, 0, true)),
-    PLAYER(new Animation("PED", "WALK_PLAYER", 4.1f, true, true, true, false, true, 0, true)),
-    WOMAN_NORMAL(new Animation("PED", "WOMAN_WALKNORM", 4.1f, true, true, true, false, true, 0, true)),
-    WOMAN_PRO(new Animation("PED", "WOMAN_WALKPRO", 4.1f, true, true, true, false, true, 0, true));
 
+    GANG1(1, new Animation("PED", "WALK_GANG1", 4.1f, true, true, true, false, true, 0, true)),
+    GANG2(2, new Animation("PED", "WALK_GANG2", 4.1f, true, true, true, false, true, 0, true)),
+    FAT(3, new Animation("PED", "WALK_FATWALK", 4.1f, true, true, true, false, true, 0, true)),
+    WUZI(4, new Animation("PED", "WUZI_WALK", 4.1f, true, true, true, false, true, 0, true)),
+    WUZI2(5, new Animation("PED", "WALK_WUZI", 4.1f, true, true, true, false, true, 0, true)),
+    PLAYER(6, new Animation("PED", "WALK_PLAYER", 4.1f, true, true, true, false, true, 0, true)),
+    WOMAN_NORMAL(7, new Animation("PED", "WOMAN_WALKNORM", 4.1f, true, true, true, false, true, 0, true)),
+    WOMAN_PRO(8, new Animation("PED", "WOMAN_WALKPRO", 4.1f, true, true, true, false, true, 0, true));
+
+    int id;
     Animation animation;
 
-    WalkStyle(Animation animation) {
+    WalkStyle(int id, Animation animation) {
         this.animation = animation;
+        this.id = id;
     }
 
     public Animation getAnimation() {
         return animation;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    static WalkStyle getById(int id) {
+        for(WalkStyle w : values()) {
+            if(w.getId() == id)
+                return w;
+        }
+        return null;
     }
 }
