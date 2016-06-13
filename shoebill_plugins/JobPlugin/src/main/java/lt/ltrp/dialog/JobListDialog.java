bc69422d664007c47e9edbe994fe906447b8c417
="Justas <justas963@gmail.com>
2016-06-13 20:01:20 +0300

    Patogesnis JobListDialog naudojimas

--- shoebill_plugins/JobPlugin/src/main/java/lt/ltrp/dialog/JobListDialog.java
+++ shoebill_plugins/JobPlugin/src/main/java/lt/ltrp/dialog/JobListDialog.java
@@ -9,8 +9,15 @@ import net.gtaun.util.event.EventManager;
 /**
  * @author Bebras
  *         2016.03.01.
+ *
 */
 public class JobListDialog extends PageListDialog {
+
+    public static AbstractJobListDialogBuilder create(LtrpPlayer player, EventManager eventManager) {
+        return new JobListDialogBuilder(new JobListDialog(player, eventManager));
+    }
+
+    private SelectJobHandler handler;
 
     public JobListDialog(LtrpPlayer player, EventManager eventManager) {
         super(player, eventManager);
@@ -19,7 +26,9 @@ public class JobListDialog extends PageListDialog {
         super.setButtonCancel("Atgal");
     }
 
-
+    public void setSelectHandler(SelectJobHandler handler) {
+        this.handler = handler;
+    }
 
     @Override
     public void show() {
@@ -32,6 +41,40 @@ public class JobListDialog extends PageListDialog {
             items.add(item);
         }
         super.show();
+    }
+
+    @Override
+    protected void onClickOk(ListDialogItem item) {
+        if(handler != null)
+            handler.onSelectJob(this, (Job)item.getData());
+        else
+            super.onClickOk(item);
+    }
+
+    @FunctionalInterface
+    public interface SelectJobHandler {
+        void onSelectJob(JobListDialog dialog, Job job);
+    }
+
+    @SuppressWarnings("unchecked")
+    public static class AbstractJobListDialogBuilder<DialogType extends JobListDialog, DialogBuilderType extends AbstractJobListDialogBuilder>
+        extends AbstractPageListDialogBuilder<DialogType, AbstractJobListDialogBuilder<DialogType, DialogBuilderType>> {
+
+        public DialogBuilderType selectJobHandler(SelectJobHandler handler) {
+            dialog.setSelectHandler(handler);
+            return (DialogBuilderType)this;
+        }
+
+        protected AbstractJobListDialogBuilder(DialogType dialog) {
+            super(dialog);
+        }
+    }
+
+    private static class JobListDialogBuilder extends AbstractJobListDialogBuilder<JobListDialog, AbstractJobListDialogBuilder> {
+
+        protected JobListDialogBuilder(JobListDialog dialog) {
+            super(dialog);
+        }
     }
 
 }
