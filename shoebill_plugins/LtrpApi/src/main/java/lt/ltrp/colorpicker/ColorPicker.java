0debef7f508e12f8bf466fd7ed8e38286aab06ec
="Justas <justas963@gmail.com>
2016-06-14 16:04:21 +0300

    Vienoj vietoje buvo nepatogu naudoti ColorPicker. Tai perrašiau jį visą. Dabar veikia(tikiuos) Shoebill Dialog stiliumi. Taip pat VehicleColorPicker skirtas transporto priemonių spalvoms.

--- shoebill_plugins/LtrpApi/src/main/java/lt/ltrp/colorpicker/ColorPicker.java
+++ shoebill_plugins/LtrpApi/src/main/java/lt/ltrp/colorpicker/ColorPicker.java
@@ -10,83 +10,70 @@ import net.gtaun.shoebill.object.Textdraw;
 import net.gtaun.util.event.EventManager;
 import net.gtaun.util.event.EventManagerNode;
 
+import java.util.List;
+
 /**
  * @author Bebras
  *         2016.02.08.
+ *
  */
 public class ColorPicker {
 
-    private static final Textdraw[] TEXTDRAWS = new Textdraw[VehicleColor.getColors().length];
-    private static final Textdraw NEXT_PAGE, PREV_PAGE, EXIT_BUTTON;
-    private static final float X = 220f;
-    private static final float Y = 100f;
-    private static final float MARGIN = 1f;
-    private static final int ITEMS_PER_PAGE = 25;
-    private static final int MAX_PAGE = TEXTDRAWS.length / ITEMS_PER_PAGE;
+    private static final ColorPickerTextDraws TEXT_DRAWS = new ColorPickerTextDraws();
 
-    static {
-        float x = X + MARGIN;
-        float y = Y + MARGIN;
-        int count = 0;
-        for(Color c : VehicleColor.getColors()) {
-            System.out.println("COUNT: " + count + " id:" + VehicleColor.getIdFromColor(c));
-            TEXTDRAWS[count] = Textdraw.create(x, y, " ");
-            TEXTDRAWS[count].setFont(TextDrawFont.MODEL_PREVIEW);
-            TEXTDRAWS[count].setPreviewModel(492);
-            TEXTDRAWS[count].setPreviewModelRotation(0f, 0f, 45f, 0.8f);
-            TEXTDRAWS[count].setPreviewVehicleColor(VehicleColor.getIdFromColor(c), VehicleColor.getIdFromColor(c));
-            TEXTDRAWS[count].setBackgroundColor(new Color(0, 0, 0, 0x77));
-            TEXTDRAWS[count].setTextSize(40f, 40f);
-            TEXTDRAWS[count].setSelectable(true);
-            x += 40f + MARGIN;
-            count++;
-            if(count > 0 && count % 5 == 0) {
-                x = X + MARGIN;
-                y += 40f + MARGIN;
-            }
-            if(count % ITEMS_PER_PAGE == 0) {
-                x = X + MARGIN;
-                y = Y + MARGIN;
-            }
-        }
-        x = 5 * MARGIN + 40 * 5 + 30f;
-        y = 5 * MARGIN + 40 * 5 + 110f;
-        PREV_PAGE = Textdraw.create(x, y, "Atgal");
-        PREV_PAGE.setFont(TextDrawFont.BANK_GOTHIC);
-        PREV_PAGE.setShadowSize(0);
-        PREV_PAGE.setTextSize(x+80f, 15f);
-        PREV_PAGE.setSelectable(true);
+   // private static final Textdraw[] TEXTDRAWS = new Textdraw[VehicleColor.getColors().length];
 
-        x += 120f;
-        NEXT_PAGE = Textdraw.create(x, y, "Toliau");
-        NEXT_PAGE.setFont(TextDrawFont.BANK_GOTHIC);
-        NEXT_PAGE.setShadowSize(0);
-        NEXT_PAGE.setTextSize(x+80f, 8f);
-        NEXT_PAGE.setSelectable(true);
-
-        x = X + (MARGIN + 40) * 5 + 3f;
-        EXIT_BUTTON = Textdraw.create(x ,Y-5f, "LD_BEAT:cross");
-        EXIT_BUTTON.setFont(TextDrawFont.SPRITE_DRAW);
-        EXIT_BUTTON.setTextSize(15f, 15f);
-        EXIT_BUTTON.setSelectable(true);
-    }
 
     private EventManagerNode eventManagerNode;
     private LtrpPlayer player;
     private ColorPickHandler handler;
+    private ColorPickerCancelHandler cancelHandler;
     private int currentPage;
+    private List<Color> colors;
 
-    public static ColorPicker create(LtrpPlayer player, EventManager manager, ColorPickHandler callback) {
-        return new ColorPicker(player, manager.createChildNode(), callback);
+    @Deprecated
+    public static AbstractColorPickerBuilder create(LtrpPlayer player, EventManager manager, ColorPickHandler callback) {
+        return new ColorPickerBuilder(new ColorPicker(player, manager.createChildNode(), callback));
     }
 
-    private ColorPicker(LtrpPlayer player, EventManagerNode node, ColorPickHandler callback) {
+    public static AbstractColorPickerBuilder create(LtrpPlayer player, EventManager eventManager, List<Color> colors) {
+        return new ColorPickerBuilder(new ColorPicker(player ,eventManager, colors));
+    }
+
+    @Deprecated
+    protected ColorPicker(LtrpPlayer player, EventManagerNode node, ColorPickHandler callback) {
         this.player = player;
         this.eventManagerNode = node;
         this.handler = callback;
     }
 
+    protected ColorPicker(LtrpPlayer player, EventManager eventManager, List<Color> colors) {
+        this.player = player;
+        this.eventManagerNode = eventManager.createChildNode();
+        this.colors = colors;
+    }
+
+
+    public void setSelectHandler(ColorPickHandler handler) {
+        this.handler = handler;
+    }
+
+    public void setCancelHandler(ColorPickerCancelHandler cancelHandler) {
+        this.cancelHandler = cancelHandler;
+    }
+
+    public void addColor(Color color) {
+        colors.add(color);
+    }
+
     public void show() {
+        int index = 0;
+        for (Color color : colors) {
+            if(index > ColorPickerTextDraws.MAX_COLORS)
+                break;
+            TEXT_DRAWS.colorTextDraws[index++].setBackgroundColor(color);
+        }
+
         show(0);
         player.selectTextDraw(Color.YELLOW);
         eventManagerNode.registerHandler(PlayerClickTextDrawEvent.class, e -> {
@@ -94,65 +81,179 @@ public class ColorPicker {
                 hide();
                 return;
             }
-            for(int i = currentPage * ITEMS_PER_PAGE; i < (currentPage+1) * ITEMS_PER_PAGE && i < TEXTDRAWS.length; i++) {
-                if(e.getTextdraw().equals(TEXTDRAWS[i])) {
+            for(int i = currentPage * ColorPickerTextDraws.ITEMS_PER_PAGE; i < (currentPage+1) * ColorPickerTextDraws.ITEMS_PER_PAGE && i < TEXT_DRAWS.colorTextDraws.length; i++) {
+                if(e.getTextdraw().equals(TEXT_DRAWS.colorTextDraws[i])) {
                     hide();
                     if(handler != null) {
-                        handler.colorSelected(i);
+                        handler.colorSelected(this, i);
                     }
                 }
             }
-            if(e.getTextdraw().equals(NEXT_PAGE) && currentPage < MAX_PAGE) {
+            if(e.getTextdraw().equals(TEXT_DRAWS.nextButton) && currentPage < TEXT_DRAWS.MAX_PAGE) {
                 show(currentPage + 1);
-            } else if(e.getTextdraw().equals(PREV_PAGE) && currentPage > 0) {
+            } else if(e.getTextdraw().equals(TEXT_DRAWS.prevButton) && currentPage > 0) {
                 show(currentPage - 1);
-            } else if(e.getTextdraw().equals(EXIT_BUTTON)) {
+            } else if(e.getTextdraw().equals(TEXT_DRAWS.exitButton)) {
+                if(cancelHandler != null) cancelHandler.onColorPickCancel(this);
                 hide();
             }
         });
     }
 
-    public void show(int page) {
+    protected void show(int page) {
         currentPage = page;
-        for(int i = page * ITEMS_PER_PAGE; i < (page + 1) * ITEMS_PER_PAGE; i++) {
-            int prevPageTdIndex = i - ITEMS_PER_PAGE,
-                    nextPageTdIndex = i + ITEMS_PER_PAGE;
-            if(prevPageTdIndex >= 0 && TEXTDRAWS[prevPageTdIndex].isShowed(player)) {
-                TEXTDRAWS[prevPageTdIndex].hide(player);
+        for(int i = page * ColorPickerTextDraws.ITEMS_PER_PAGE; i < (page + 1) * ColorPickerTextDraws.ITEMS_PER_PAGE; i++) {
+            int prevPageTdIndex = i - ColorPickerTextDraws.ITEMS_PER_PAGE,
+                    nextPageTdIndex = i + ColorPickerTextDraws.ITEMS_PER_PAGE;
+            if(prevPageTdIndex >= 0 && TEXT_DRAWS.colorTextDraws[prevPageTdIndex].isShowed(player)) {
+                TEXT_DRAWS.colorTextDraws[prevPageTdIndex].hide(player);
             }
-            if(nextPageTdIndex < TEXTDRAWS.length && TEXTDRAWS[nextPageTdIndex].isShowed(player)) {
-                TEXTDRAWS[nextPageTdIndex].hide(player);
+            if(nextPageTdIndex < TEXT_DRAWS.colorTextDraws.length && TEXT_DRAWS.colorTextDraws[nextPageTdIndex].isShowed(player)) {
+                TEXT_DRAWS.colorTextDraws[nextPageTdIndex].hide(player);
             }
-            if(i < TEXTDRAWS.length)
-                TEXTDRAWS[i].show(player);
+            if(i < TEXT_DRAWS.colorTextDraws.length)
+                TEXT_DRAWS.colorTextDraws[i].show(player);
         }
-        if(!PREV_PAGE.isShowed(player)) {
-            PREV_PAGE.show(player);
+        if(!TEXT_DRAWS.prevButton.isShowed(player)) {
+            TEXT_DRAWS.prevButton.show(player);
         }
-        if(!NEXT_PAGE.isShowed(player)) {
-            NEXT_PAGE.show(player);
+        if(!TEXT_DRAWS.nextButton.isShowed(player)) {
+            TEXT_DRAWS.nextButton.show(player);
         }
-        if(!EXIT_BUTTON.isShowed(player))
-            EXIT_BUTTON.show(player);
+        if(!TEXT_DRAWS.exitButton.isShowed(player))
+            TEXT_DRAWS.exitButton.show(player);
     }
 
     public void hide() {
         player.cancelSelectTextDraw();
-        for(Textdraw td : TEXTDRAWS) {
+        for(Textdraw td : TEXT_DRAWS.colorTextDraws) {
             if(td.isShowed(player)) {
                 td.hide(player);
             }
         }
-        NEXT_PAGE.hide(player);
-        PREV_PAGE.hide(player);
-        EXIT_BUTTON.hide(player);
+        TEXT_DRAWS.nextButton.hide(player);
+        TEXT_DRAWS.prevButton.hide(player);
+        TEXT_DRAWS.exitButton.hide(player);
         eventManagerNode.cancelAll();
     }
 
+    @SuppressWarnings("unchecked")
+    public static class AbstractColorPickerBuilder<ColorPickerType extends ColorPicker, ColorPickerBuilderType extends AbstractColorPickerBuilder> {
+
+        private ColorPickerType colorPicker;
+
+        public AbstractColorPickerBuilder(ColorPickerType colorPicker) {
+            this.colorPicker = colorPicker;
+        }
+
+        public ColorPickerBuilderType color(Color c) {
+            colorPicker.addColor(c);
+            return (ColorPickerBuilderType)this;
+        }
+
+        public ColorPickerBuilderType onSelectColor(ColorPickHandler handler) {
+            colorPicker.setSelectHandler(handler);
+            return (ColorPickerBuilderType)this;
+        }
+
+        public ColorPickerBuilderType onCancel(ColorPickerCancelHandler handler) {
+            colorPicker.setCancelHandler(handler);
+            return (ColorPickerBuilderType)this;
+        }
+
+        public ColorPickerType build() {
+            return colorPicker;
+        }
+
+    }
+
+    private static class ColorPickerBuilder extends AbstractColorPickerBuilder<ColorPicker, ColorPickerBuilder> {
+        public ColorPickerBuilder(ColorPicker colorPicker) {
+            super(colorPicker);
+        }
+    }
 
     @FunctionalInterface
     public interface ColorPickHandler {
-        void colorSelected(int color);
+        void colorSelected(ColorPicker colorpicker, int color);
+    }
+
+    @FunctionalInterface
+    public interface ColorPickerCancelHandler {
+        void onColorPickCancel(ColorPicker picker);
+    }
+
+    static class ColorPickerTextDraws {
+        static final int MAX_COLORS = 255;
+        static final float X = 220f;
+        static final float Y = 100f;
+        static final float MARGIN = 1f;
+        static final int ITEMS_PER_PAGE = 25;
+
+
+        final int MAX_PAGE = MAX_COLORS / ITEMS_PER_PAGE;
+        Textdraw[] colorTextDraws;
+        Textdraw nextButton, prevButton, exitButton;
+
+        public ColorPickerTextDraws() {
+            colorTextDraws = new Textdraw[MAX_COLORS];
+
+            float x = X + MARGIN;
+            float y = Y + MARGIN;
+            int count = 0;
+            for(Color c : VehicleColor.getColors()) {
+                System.out.println("COUNT: " + count + " id:" + VehicleColor.getIdFromColor(c));
+                colorTextDraws[count] = Textdraw.create(x, y, " ");
+                colorTextDraws[count].setFont(TextDrawFont.MODEL_PREVIEW);
+                //colorTextDraws[count].setPreviewModel(492);
+                colorTextDraws[count].setPreviewModelRotation(0f, 0f, 45f, 0.8f);
+                //colorTextDraws[count].setPreviewVehicleColor(VehicleColor.getIdFromColor(c), VehicleColor.getIdFromColor(c));
+                colorTextDraws[count].setBackgroundColor(new Color(0, 0, 0, 0x77));
+                colorTextDraws[count].setTextSize(40f, 40f);
+                colorTextDraws[count].setSelectable(true);
+                x += 40f + MARGIN;
+                count++;
+                if(count > 0 && count % 5 == 0) {
+                    x = X + MARGIN;
+                    y += 40f + MARGIN;
+                }
+                if(count % ITEMS_PER_PAGE == 0) {
+                    x = X + MARGIN;
+                    y = Y + MARGIN;
+                }
+            }
+            x = 5 * MARGIN + 40 * 5 + 30f;
+            y = 5 * MARGIN + 40 * 5 + 110f;
+            prevButton = Textdraw.create(x, y, "Atgal");
+            prevButton.setFont(TextDrawFont.BANK_GOTHIC);
+            prevButton.setShadowSize(0);
+            prevButton.setTextSize(x+80f, 15f);
+            prevButton.setSelectable(true);
+
+            x += 120f;
+            nextButton = Textdraw.create(x, y, "Toliau");
+            nextButton.setFont(TextDrawFont.BANK_GOTHIC);
+            nextButton.setShadowSize(0);
+            nextButton.setTextSize(x+80f, 8f);
+            nextButton.setSelectable(true);
+
+            x = X + (MARGIN + 40) * 5 + 3f;
+            exitButton = Textdraw.create(x ,Y-5f, "LD_BEAT:cross");
+            exitButton.setFont(TextDrawFont.SPRITE_DRAW);
+            exitButton.setTextSize(15f, 15f);
+            exitButton.setSelectable(true);
+        }
+
+        @Override
+        protected void finalize() throws Throwable {
+            for(Textdraw td : colorTextDraws) {
+                td.destroy();
+            }
+            nextButton.destroy();
+            prevButton.destroy();
+            exitButton.destroy();
+            super.finalize();
+        }
     }
 
 
