eb7213a19c19276c2ea0c94483b97d753cf68921
Bebras <justas963@gmail.com>
2016-10-14 23:05:29 +0200

    Kodėl šitie dialogai necomittinti?

---
+++ shoebill_plugins/LtrpApi/src/main/java/lt/ltrp/player/dialog/PlayerDescriptionInputDialog.java
@@ -0,0 +1,37 @@
+package lt.ltrp.player.dialog;
+
+import lt.ltrp.data.Color;
+import lt.ltrp.object.LtrpPlayer;
+import lt.ltrp.player.PlayerController;
+import lt.ltrp.util.StringUtils;
+import net.gtaun.shoebill.common.dialog.AbstractDialog;
+import net.gtaun.shoebill.common.dialog.InputDialog;
+import net.gtaun.util.event.EventManager;
+
+/**
+ * @author Bebras
+ *         2016.06.01.
+ *
+ *         This dialog appends text to a target players description
+ */
+public class PlayerDescriptionInputDialog {
+
+    public static InputDialog create(LtrpPlayer player, EventManager eventManager, AbstractDialog parent, LtrpPlayer target) {
+        return InputDialog.create(player, eventManager)
+                .caption(target.getCharName() + " veikëjo apraðymo pildymas")
+                .parentDialog(parent)
+                .onClickCancel(AbstractDialog::showParentDialog)
+                .buttonOk("Saugoti")
+                .buttonCancel("Atgal")
+                .line("Áraðykite, kà norite pridëti prie veikëjo apraðymo")
+                .line("\nDabartinis tekstas:")
+                .line(StringUtils.addLineBreaks(target.getDescription(), 60))
+                .onClickOk((d, s) -> {
+                    target.setDescription(target.getDescription() + s);
+                    PlayerController.instance.update(target);
+                    player.sendMessage(Color.NEWS, "Apraðymas sëkmingai papildytas");
+                    if(parent != null) parent.show();
+                })
+                .build();
+    }
+}
