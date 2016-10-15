41a7443f9c1ae52333f2f989b2c4d0544f398ee7
Bebras <justas963@gmail.com>
2016-10-15 13:54:00 +0200

    Redagavus nustatymus bus iššaunamas atitinkamas event

--- shoebill_plugins/player-settings/src/main/java/lt/ltrp/player/settings/PlayerSettingsControllerImpl.kt
+++ shoebill_plugins/player-settings/src/main/java/lt/ltrp/player/settings/PlayerSettingsControllerImpl.kt
@@ -1,17 +1,22 @@
 package lt.ltrp.player.settings
 
+import lt.ltrp.`object`.LtrpPlayer
 import lt.ltrp.player.settings.dao.PlayerSettingsDao
 import lt.ltrp.player.settings.data.PlayerSettings
+import lt.ltrp.player.settings.event.PlayerEditSettingsEvent
+import net.gtaun.util.event.EventManager
 
 /**
  * Created by Bebras on 2016-10-15.
  * Provides implementation for [PlayerSettingsController] interface
  */
-class PlayerSettingsControllerImpl(private val settingsDao: PlayerSettingsDao): PlayerSettingsController() {
+class PlayerSettingsControllerImpl(private val settingsDao: PlayerSettingsDao, private val eventManager: EventManager):
+        PlayerSettingsController() {
 
 
     override fun update(settings: PlayerSettings) {
         settingsDao.update(settings)
+        eventManager.dispatchEvent(PlayerEditSettingsEvent(settings.player as? LtrpPlayer, settings))
     }
 
 }
