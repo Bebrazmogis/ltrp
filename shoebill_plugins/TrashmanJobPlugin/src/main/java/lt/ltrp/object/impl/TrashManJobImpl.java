bb4f37e610d4adce7f7c603f744a69ef263a712e
Bebras <justas963@gmail.com>
2016-11-09 14:20:14 +0100

    Pašalintas nereikalingas konstruktorius

--- shoebill_plugins/TrashmanJobPlugin/src/main/java/lt/ltrp/object/impl/TrashManJobImpl.java
+++ shoebill_plugins/TrashmanJobPlugin/src/main/java/lt/ltrp/object/impl/TrashManJobImpl.java
@@ -26,14 +26,8 @@ public class TrashManJobImpl extends AbstractContractJob implements TrashManJob 
 
     private TrashMissions trashMissions;
 
-    public TrashManJobImpl(int id, String name, Location location, int basePaycheck, EventManager eventManager, int contractLength, int maxPaycheck, int trashMasterCapacity, int trashRouteBonus) {
-        super(id, name, location, basePaycheck, eventManager, contractLength, maxPaycheck);
-        this.trashMasterCapacity = trashMasterCapacity;
-        this.trashRouteBonus = trashRouteBonus;
-    }
-
     public TrashManJobImpl(int id, EventManager eventManager) {
-        this(id, null, null, 0, eventManager, 0, 0, 0, 0);
+        super(id, eventManager);
     }
 
     public int getTrashMasterCapacity() {
