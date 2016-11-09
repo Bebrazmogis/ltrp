9dce3e016ecac539b7fa91e1fdbb477e855243d8
Bebras <justas963@gmail.com>
2016-11-09 11:29:25 +0100

    Įėjimų plugino sql

---
+++ shoebill_plugins/EntrancePlugin/src/main/resources/database.sql
@@ -0,0 +1,22 @@
+UPDATE entrances SET name = ?, x = ?, y = ?, z = ?, angle = ?, interior = ?, virtual_world = ?,
+label_color = ?, pickup_model = ?, job_id = ?, is_vehicle = ?, exit_id = ? WHERE id = ?
+
+
+
+create table if not exists entrances
+(
+    id int unsigned auto_increment not null,
+    name varchar(128) not null,
+    x float not null,
+    y float not null,
+    z float not null,
+    angle float not null,
+    interior int not null,
+    virtual_world int not null,
+    label_color int not null,
+    pickup_model smallint unsigned not null,
+    job_id int null,
+    is_vehicle tinyint unsigned not null,
+    exit_id int null,
+    primary key(id)
+) engine=innodb default charset=utf8 collate=utf8_unicode_ci;
