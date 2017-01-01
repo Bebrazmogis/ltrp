7f5356fc2906915257ccaa8996d3b9adb51cb27e
="Justas <justas963@gmail.com>
2017-01-01 18:44:05 +0200

    Redaguoti laikomi duomenys apie žaidėja. LtrpPlayer nuspręsta, nebeextendins Player o turės fielda jam.

--- shoebill_libs/LtrpApi/src/main/java/lt/ltrp/object/LtrpPlayer.kt
+++ shoebill_libs/LtrpApi/src/main/java/lt/ltrp/object/LtrpPlayer.kt
@@ -1,6 +1,8 @@
 package lt.ltrp.`object`
 
 
+import lt.ltrp.ActionMessenger
+import lt.ltrp.StateMessenger
 import lt.ltrp.constant.MODERATOR
 import lt.ltrp.data.Animation
 import lt.ltrp.data.LtrpWeaponData
@@ -17,11 +19,125 @@ import java.util.*
 /**
  * @author Bebras
  *         2016.04.07.
+ *         The main player object that holds ALL the information about a player
+ *         This object represents a connected player, for offline player data refer to [PlayerData]
+ *         It should NOT extend [Player] class as it has a field for accessing shoebill/samp data
  */
-abstract class LtrpPlayer() : Player(), PlayerData {
+interface LtrpPlayer : PlayerData, StateMessenger, ActionMessenger {
 
-    var isModerator: Boolean = false
-        get() { return modLevel > 0 }
+    val player: Player
+    val isModerator: Boolean
+        get() = modLevel > 0
+    val isAdmin: Boolean
+        get() = player.isAdmin || adminLevel > 0
+
+
+    val ucpId: Int
+
+    fun sendInfoText(msg: String)  {
+        sendInfoText(msg, DEFAULT_INFOTEXT_DURATION)
+    }
+
+    fun sendInfoText(msg: String, seconds: Int)
+
+    fun getWeapons(): Array<LtrpWeaponData>
+    fun getWeaponData(weaponModel: WeaponModel): LtrpWeaponData
+    fun ownsWeapon(model: WeaponModel): Boolean
+    fun isWeaponSlotUsed(slot: WeaponSlot): Boolean
+    fun removeWeapon(weaponData: LtrpWeaponData)
+    fun removeWeapon(model: WeaponModel)
+    fun removeJobWeapons()
+    fun getArmedWeaponData(): LtrpWeaponData
+    fun giveWeapon(weaponData: LtrpWeaponData)
+
+    fun setVolume(volume: Int)
+    fun isDataLoaded(): Boolean
+
+    fun getOffers(): Collection<PlayerOffer>
+    fun containsOffer(type: Class<*>): Boolean
+    fun <T : PlayerOffer> getOffers(type: Class<T>): Collection<T>
+    fun <T : PlayerOffer> getOffer(type: Class<T>): T
+
+    fun sendFadeMessage(color: Color, text: String, distance: Float)
+    /**
+     * Sends a message to the player but fades the colour accordingly from players distance from location
+     */
+    fun sendFadeMessage(color: Color, text: String, location: Location)
+    fun sendErrorMessage(message: String)
+    fun sendErrorMessage(errorCode: Int)
+    fun sendMessage(s: String, distance: Float) {
+        sendMessage(Color.WHITE, s, distance)
+    }
+
+    fun sendMessage(color: Color, s: String, distance: Float)
+    fun sendDebug(color: Color, message: String)
+    fun sendDebug(message: String)
+    fun sendDebug(vararg objects: Any)
+
+    /**
+     * Just a wrapper for [Player.sendMessage] for convenience
+     */
+    fun sendMessage(color: Color, message: String) {
+        player.sendMessage(color, message)
+    }
+
+
+    fun getClosestPlayer(maxDistance: Float): LtrpPlayer
+    fun getClosestPlayer(): LtrpPlayer
+    fun getClosestPlayers(maxDistance: Float): Array<LtrpPlayer>
+    fun applyAnimation(animation: Animation)
+    fun applyLoopAnimation(animLib: String, animation: String, lockX: Boolean, lockY: Boolean, stoppable: Boolean) {
+        applyLoopAnimation(animLib, animation, lockX, lockY, false, stoppable)
+    }
+
+    /**
+     * Deprecated because of freeze parameter, it does not make sense to play a loop and then freeze.
+     * @param animLib
+     * @param animation
+     * @param lockX
+     * @param lockY
+     * @param freeze
+     * @param stoppable
+     */
+    @Deprecated(message = "")
+    fun applyLoopAnimation(animLib: String, animation: String, lockX: Boolean, lockY: Boolean, freeze: Boolean, stoppable: Boolean) {
+        applyAnimation(animLib, animation, 4.1f, true, lockX, lockY, freeze, stoppable)
+    }
+
+    fun applyAnimation(animLib: String, anim: String, speed: Float, loop: Boolean, lockX: Boolean, lockY: Boolean, freeze: Boolean, time: Int, forceSync: Boolean) {
+        applyAnimation(animLib, anim, speed, loop, lockX, lockY, freeze, time, forceSync, false)
+    }
+
+    fun applyAnimation(animLib: String, anim: String, speeed: Float, loop: Boolean, lockX: Boolean, lockY: Boolean, freeze: Boolean, time: Int, forceSync: Boolean, stopable: Boolean)
+    fun applyAnimation(animLib: String, anim: String, lockX: Boolean, lockY: Boolean, freeze: Boolean, time: Int, stoppable: Boolean) {
+        applyAnimation(animLib, anim, 4.1f, false, lockX, lockY, freeze, time, true, stoppable)
+    }
+
+    fun clearAnimations()
+    fun getAnimation(): Animation
+
+    /**
+
+     * @param animLib
+     * @param animName
+     * @param speed
+     * @param loop
+     * @param lockX
+     * @param lockY
+     * @param freeze
+     */
+    fun applyAnimation(animLib: String, animName: String, speed: Float, loop: Boolean, lockX: Boolean, lockY: Boolean, freeze: Boolean) {
+        applyAnimation(animLib, animName, speed, loop, lockX, lockY, freeze, false)
+    }
+
+    fun applyAnimation(animLib: String, animName: String, speed: Float, loop: Boolean, lockX: Boolean, lockY: Boolean, freeze: Boolean, stoppable: Boolean)
+    fun isAudioConnected(): Boolean
+
+    fun freeze()
+    fun unfreeze()
+
+    fun mute()
+    fun unMute()
 
     companion object {
         val DEFAULT_PLAYER_COLOR = Color(0xFF, 0xFF, 0xFF, 0x00)
@@ -47,26 +163,26 @@ abstract class LtrpPlayer() : Player(), PlayerData {
 
         fun getByPartName(name: String): LtrpPlayer? {
             val unmatchedChars: SortedMap<Int, LtrpPlayer> = TreeMap()
-           get().filter { it.name.contains(name) }.forEach {
-               unmatchedChars.put(it.name.length - name.length, it)
-           }
+            get().filter { it.name.contains(name) }.forEach {
+                unmatchedChars.put(it.name.length - name.length, it)
+            }
             return unmatchedChars.values.first()
         }
 
         fun getClosest(player: Player, maxDistance: Float): LtrpPlayer? {
             return get()
-                    .filter { it.location.distance(player.location) <= maxDistance }
-                    .minBy { it.location.distance(player.location) }
+                    .filter { it.player.location.distance(player.location) <= maxDistance }
+                    .minBy { it.player.location.distance(player.location) }
         }
 
         fun getClosestPlayers(player: Player, maxDistance: Float): List<LtrpPlayer> {
-            return get().filter { it.location.distance(player.location) <= maxDistance }
+            return get().filter { it.player.location.distance(player.location) <= maxDistance }
         }
 
         fun sendAdminMessage(s: String) {
             get()
                     .filter { it.isAdmin }
-                    .forEach { it.sendMessage(Color.GREENYELLOW, s) }
+                    .forEach { it.player.sendMessage(Color.GREENYELLOW, s) }
         }
 
         /**
@@ -100,110 +216,4 @@ abstract class LtrpPlayer() : Player(), PlayerData {
             get().forEach { it.sendMessage(color, message) }
         }
     }
-
-    fun sendInfoText(msg: String)  {
-        sendInfoText(msg, DEFAULT_INFOTEXT_DURATION);
-    }
-
-    internal abstract fun sendInfoText(msg: String, seconds: Int)
-
-    abstract fun getWeapons(): Array<LtrpWeaponData>
-    abstract fun getWeaponData(weaponModel: WeaponModel): LtrpWeaponData
-    abstract fun ownsWeapon(model: WeaponModel): Boolean
-    abstract fun isWeaponSlotUsed(slot: WeaponSlot): Boolean
-    abstract fun removeWeapon(weaponData: LtrpWeaponData)
-    abstract fun removeWeapon(model: WeaponModel)
-    abstract fun removeJobWeapons()
-    abstract fun getArmedWeaponData(): LtrpWeaponData
-    abstract fun giveWeapon(weaponData: LtrpWeaponData)
-
-    abstract fun setVolume(volume: Int)
-
-    abstract fun isDataLoaded(): Boolean
-
-    abstract fun getOffers(): Collection<PlayerOffer>
-    abstract fun containsOffer(type: Class<*>): Boolean
-    abstract fun <T : PlayerOffer> getOffers(type: Class<T>): Collection<T>
-    abstract fun <T : PlayerOffer> getOffer(type: Class<T>): T
-
-    abstract fun sendFadeMessage(color: Color, text: String, distance: Float)
-    /**
-     * Sends a message to the player but fades the colour accordingly from players distance from location
-     */
-    abstract fun sendFadeMessage(color: Color, text: String, location: Location)
-    abstract fun sendErrorMessage(message: String)
-    abstract fun sendErrorMessage(errorCode: Int)
-    abstract fun sendActionMessage(message: String, distance: Float)
-    abstract fun sendActionMessage(s: String)
-    abstract fun sendStateMessage(s: String, distance: Float)
-    abstract fun sendStateMessage(s: String)
-    fun sendMessage(s: String, distance: Float) {
-        sendMessage(Color.WHITE, s, distance)
-    }
-
-    abstract fun sendMessage(color: Color, s: String, distance: Float)
-    abstract fun sendDebug(color: Color, message: String)
-    abstract fun sendDebug(message: String)
-    abstract fun sendDebug(vararg objects: Any)
-
-
-    abstract fun getClosestPlayer(maxDistance: Float): LtrpPlayer
-    abstract fun getClosestPlayer(): LtrpPlayer
-    abstract fun getClosestPlayers(maxDistance: Float): Array<LtrpPlayer>
-    abstract fun applyAnimation(animation: Animation)
-    fun applyLoopAnimation(animLib: String, animation: String, lockX: Boolean, lockY: Boolean, stoppable: Boolean) {
-        applyLoopAnimation(animLib, animation, lockX, lockY, false, stoppable)
-    }
-
-    /**
-     * Deprecated because of freeze parameter, it does not make sense to play a loop and then freeze.
-     * @param animLib
-     * @param animation
-     * @param lockX
-     * @param lockY
-     * @param freeze
-     * @param stoppable
-     */
-    @Deprecated(message = "")
-    fun applyLoopAnimation(animLib: String, animation: String, lockX: Boolean, lockY: Boolean, freeze: Boolean, stoppable: Boolean) {
-        applyAnimation(animLib, animation, 4.1f, true, lockX, lockY, freeze, stoppable)
-    }
-
-    fun applyAnimation(animLib: String, anim: String, speed: Float, loop: Boolean, lockX: Boolean, lockY: Boolean, freeze: Boolean, time: Int, forceSync: Boolean) {
-        applyAnimation(animLib, anim, speed, loop, lockX, lockY, freeze, time, forceSync, false)
-    }
-
-    abstract fun applyAnimation(animLib: String, anim: String, speeed: Float, loop: Boolean, lockX: Boolean, lockY: Boolean, freeze: Boolean, time: Int, forceSync: Boolean, stopable: Boolean)
-    fun applyAnimation(animLib: String, anim: String, lockX: Boolean, lockY: Boolean, freeze: Boolean, time: Int, stoppable: Boolean) {
-        applyAnimation(animLib, anim, 4.1f, false, lockX, lockY, freeze, time, true, stoppable)
-    }
-
-    abstract fun clearAnimations()
-    abstract fun getAnimation(): Animation
-
-    /**
-
-     * @param animLib
-     * @param animName
-     * @param speed
-     * @param loop
-     * @param lockX
-     * @param lockY
-     * @param freeze
-     */
-    fun applyAnimation(animLib: String, animName: String, speed: Float, loop: Boolean, lockX: Boolean, lockY: Boolean, freeze: Boolean) {
-        applyAnimation(animLib, animName, speed, loop, lockX, lockY, freeze, false)
-    }
-
-    abstract fun applyAnimation(animLib: String, animName: String, speed: Float, loop: Boolean, lockX: Boolean, lockY: Boolean, freeze: Boolean, stoppable: Boolean)
-    abstract fun isAudioConnected(): Boolean
-
-    abstract fun getUcpId(): Int
-    abstract fun setUcpId(ucpId: Int)
-
-    abstract fun freeze()
-    abstract fun unfreeze()
-
-    abstract fun mute()
-    abstract fun unMute()
 }
--- shoebill_libs/LtrpApi/src/main/java/lt/ltrp/object/PlayerData.kt
+++ shoebill_libs/LtrpApi/src/main/java/lt/ltrp/object/PlayerData.kt
@@ -6,6 +6,7 @@ import lt.ltrp.data.PlayerDrugs
 import lt.ltrp.player.licenses.data.PlayerLicenses
 import lt.ltrp.player.settings.data.PlayerSettings
 import lt.maze.audio.AudioHandle
+import net.gtaun.shoebill.entities.Vehicle
 import java.lang.ref.WeakReference
 import java.time.LocalDateTime
 
@@ -15,8 +16,8 @@ import java.time.LocalDateTime
 interface PlayerData: InventoryEntity {
 
     var password: String
-    var secretQuestion: String
-    var secretAnswer: String
+    var secretQuestion: String?
+    var secretAnswer: String?
     var level: Int
     var adminLevel: Int
     var modLevel: Int
@@ -30,13 +31,11 @@ interface PlayerData: InventoryEntity {
     var sex: String
     var age: Int
     var origin: String
-    var disease: Int
-    var respect: Int
     var deaths: Int
     var walkStyle: WalkStyle
     var talkStyle: TalkStyle
     var lastLogin: LocalDateTime
-    var lastUsedVehicle: LtrpVehicle?
+    var lastUsedVehicle: Vehicle?
     var hunger: Int
     var boxingStyle: Int
     var forumName: String
@@ -75,7 +74,7 @@ interface PlayerData: InventoryEntity {
     var isMod: Boolean
 
 
-    val countdown: PlayerCountdown
+    val countdown: PlayerCountdown?
     val infoBox: PlayerInfoBox
     fun addTotalPaycheck(amount: Int)
 
