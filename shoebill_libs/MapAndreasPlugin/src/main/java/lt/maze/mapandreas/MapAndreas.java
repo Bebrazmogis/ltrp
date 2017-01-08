868c23c5e9c3f56f55b4d04c82f4bb30eba89c77
="Justas <justas963@gmail.com>
2017-01-08 17:26:34 +0200

    Atnajinta į shoebill 2.0-RC. Pašalinti static metodai, nes jei yra classpathe, nebūtinai reiškia kad pluginas buvo initialized

--- shoebill_libs/MapAndreasPlugin/src/main/java/lt/maze/mapandreas/MapAndreas.java
+++ shoebill_libs/MapAndreasPlugin/src/main/java/lt/maze/mapandreas/MapAndreas.java
@@ -1,70 +1,71 @@
 package lt.maze.mapandreas;
 
 
+import net.gtaun.shoebill.ShoebillMain;
 import net.gtaun.shoebill.amx.AmxInstance;
 import net.gtaun.shoebill.data.Vector2D;
 import net.gtaun.shoebill.resource.Plugin;
 import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 /**
  * @author Bebras
  *         2015.12.03.
  */
+@ShoebillMain(name = "Map Andreas Wrapper plugin", author = "Bebras",
+        description = "A wrapper of SAMP MapAndreas plugin\n" +
+                "http://forum.sa-mp.com/showthread.php?t=275492\n" +
+                "I merely created the wrapper, all credits for the actual C++ plugin go to Mauzen")
 public class MapAndreas extends Plugin {
 
-    private static final Logger logger = LoggerFactory.getLogger(MapAndreas.class);
-
-
     // native MapAndreas_Init(mode, name[]="", len=sizeof(name));
-    public static void init(MapAndreasMode mode, String name) {
+    public void init(MapAndreasMode mode, String name) {
         Functions.MapAndreas_Init(mode.getValue(), name);
     }
 
-    public static void init(MapAndreasMode mode) {
+    public void init(MapAndreasMode mode) {
         init(mode, "");
     }
 
 
     // native MapAndreas_Unload();
-    public static void unload() {
+    public void unload() {
         Functions.MapAndreas_Unload();
     }
 
 
     // native MapAndreas_FindZ_For2DCoord(Float:X, Float:Y, &Float:Z);
-    public static float findZ(float x, float y) {
+    public float findZ(float x, float y) {
         return Functions.MapAndreas_FindZ_For2DCoord(x, y);
     }
 
-    public static float findZ(Vector2D location) {
+    public float findZ(Vector2D location) {
         return findZ(location.x, location.y);
     }
 
     // native MapAndreas_FindAverageZ(Float:X, Float:Y, &Float:Z);
-    public static float findAverageZ(float x, float y) {
+    public float findAverageZ(float x, float y) {
         return Functions.MapAndreas_FindAverageZ(x, y);
     }
 
     // native MapAndreas_SetZ_For2DCoord(Float:X, Float:Y, Float:Z);
-    public static void setZ(float x, float y, float z) {
+    public void setZ(float x, float y, float z) {
         Functions.MapAndreas_SetZ_For2DCoord(x, y, z);
     }
 
     // native MapAndreas_SaveCurrentHMap(name[]);
-    public static void saveCurrentMap(String name) {
+    public void saveCurrentMap(String name) {
         Functions.MapAndreas_SaveCurrentHMap(name);
     }
 
     // native MapAndreas_GetAddress(); //only for plugins
-    public static int getAddress() {
+    public int getAddress() {
         return Functions.MapAndreas_GetAddress();
     }
 
 
     @Override
     protected void onEnable() throws Throwable {
-        Logger logger = LoggerFactory.getLogger(getClass());
+        Logger logger = getLogger();
         logger.debug("MapAndreas registering functions..");
         Functions.registerFunctions(AmxInstance.getDefault());
         logger.debug("MapAndreas initializing");
